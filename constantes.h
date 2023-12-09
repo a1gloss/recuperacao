2c36e6a17530a537851a5b77c974d7f509ab58f7
amanda <127759153+a1gloss@users.noreply.github.com>
2023-12-09 15:44:36 -0300

    Update constantes.h

---
+++ constantes.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+#define NUM_PRODUTOS 10
+#define COLUNAS_MATRIZ_FINAL 13
+#define LINHAS_MATRIZ_FINAL 10
+#define TAMANHO_LINHA 10
+#define MESES 12
+
+#endif
